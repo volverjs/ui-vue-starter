771a52ec838c109f3fc1020329e18108068d8c0e
francesco.bruno <>
2023-02-20 15:29:08 +0100

    Page 404

---
+++ src/pages/[...all].vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="flex flex-col items-center m-auto">
+		<h1 class="vv-text vv-text--size-1 font-serif font-bold text-center">
+			404 Error
+		</h1>
+		<p class="vv-text vv-text--size-4 text-word-3 text-center mb-lg">
+			The page <em class="italic">'{{ routeParameters }}'</em> that you
+			are looking for, may have been removed, deleted or possibly never
+			existed.
+		</p>
+		<VvButton :to="{ name: 'index' }" modifiers="full-bleed" class="w-max">
+			Back to home
+		</VvButton>
+	</div>
+</template>
+
+<script setup lang="ts">
+	import { useAttrs } from 'vue'
+
+	// get route parameters
+	const attrs = useAttrs() as { all: string[] }
+	const routeParameters = computed(() => {
+		return attrs?.all?.join('/')
+	})
+</script>
